a5bc8541ee418b1251e099de1cf1f787099cf418
Md Shahed Hossain <shahed@medisys.com.sa>
2019-12-16 11:47:03 +0800

    Initial Commit

---
+++ RufaidaGUI/RufaidaGUI.h
@@ -0,0 +1,19 @@
+//
+//  RufaidaGUI.h
+//  RufaidaGUI
+//
+//  Created by Md Shahed Hossain on 16/12/2019.
+//  Copyright © 2019 Medical Systems Company Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RufaidaGUI.
+FOUNDATION_EXPORT double RufaidaGUIVersionNumber;
+
+//! Project version string for RufaidaGUI.
+FOUNDATION_EXPORT const unsigned char RufaidaGUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RufaidaGUI/PublicHeader.h>
+
+
---
+++ RufaidaGUITests/RufaidaGUITests.swift
@@ -0,0 +1,34 @@
+//
+//  RufaidaGUITests.swift
+//  RufaidaGUITests
+//
+//  Created by Md Shahed Hossain on 16/12/2019.
+//  Copyright © 2019 Medical Systems Company Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import RufaidaGUI
+
+class RufaidaGUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
